fc0d24c9aecbb3850cbbddd75496093f05d80c04
chandrahaasJ <chandrahaasjasti@gmail.com>
2025-06-02 00:43:58 +0530

    added HRbot perception

--- classes/HR/perception.py
+++ classes/HR/perception.py
@@ -7,17 +7,18 @@ gem=os.getenv("LLM")
 gemini=os.getenv("LLM")
 client = genai.Client(api_key=gemini)
 
-prompt=""" 
-you are a Human Resources professional with expertise in employee perception.
-you asked a candidate who appeared a question which goes as: "Tell me about yourself"
-your job is to identify his expertise and skills in different fields.
-derive occupation(the candidate can also be a student), skills(e.g. programming languages,soft skills, sports) and experience(only coorporate expereince).
+with open(r"C:\ResumeGenerator\resGen\classes\HR\prompts\objective_prompt.txt","r") as f:
+    objective_prompt = f.read()
 
-GUIDELINES:
-1. If the candidate did not mention any occupation, skills or experience, ask him again what he again what he missed
-"""
+ans="""
+This is Pravalika and I am currently in my 3rd year of engineering in kmit. I’ve spent my college years learning new programming languages and experimenting with them through problem solving and projects. I have done 2 projects which come under generative ai domain. The first project’s chatbot for medical queries which focuses on giving day to day medical related information when asked by the user and to achieve that  my team and me have used an llm called Bert and rag for retrieval of the apt information. We have also used faiss that’s a Facebook ai similarly search it’s a library which helps mapping similar data together, by using both of them the retrieval process became accurate. The other project was called as FYI - for your information. This was a news generator which summarises different articles from different sources and gives you a better idea of the news topic, another feature was YouTube summarisation as well. These are the two projects I have done and I learnt a lot from these. I’ve learnt about llms, about front end, back end and all the different aspects of building a project. I am proficient in speaking skills and a good team spirit."""
+
+objective_prompt+=ans
+objective_prompt+="\n\n"
+objective_prompt+=""" FINAL INSTRUCTION:
+Be Sure to use the examples, guidelines and instructions provided in the prompt to generate the response in JSON format ONLY.\n\n"""
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.0-flash", contents=objective_prompt
 )
 print(response.text)
